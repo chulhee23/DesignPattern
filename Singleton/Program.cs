5bf92b508192bcae5fbfb94fc28767dc6e1cf0f6
chulhee23 <chulhee2351@gmail.com>
2021-07-16 09:52:48 +0900

    singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Singleton
+{
+  public sealed class Singleton
+  {
+    public static readonly Singleton Instance = new Singleton();
+
+    private Singleton() { }
+
+    public void Method()
+    {
+      Console.WriteLine("...");
+    }
+  }
+
+  class Client
+  {
+    public static void HowToTest()
+    {
+      Singleton.Instance.Method();
+    }
+  }
+
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Client.HowToTest();
+
+    }
+  }
+
+}
