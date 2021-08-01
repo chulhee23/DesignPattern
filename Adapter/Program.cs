e85ce5b8221c5a84c8bfe9ebc95d82f08d4308db
chulhee23 <chulhee2351@gmail.com>
2021-08-01 21:42:05 +0900

    Adapter pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Adapter
+{
+    public interface IAdapter {
+        void Display(byte[] data);
+    }
+
+    public class HdmiToVgaAdapter : IAdapter{
+        private LegacyVGA vga = new LegacyVGA();
+        public void Display(byte[] data){
+            // convert to vga
+            vga.Display(data);
+        }
+    }
+
+    public class LegacyVGA{
+        public void Display(byte[] data){
+            // ...
+        }
+    }
+    
+    public class HdmiAdapter : IAdapter{
+        public void Display(byte[] data){
+            // ...
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool vgaMonitor = true;
+            IAdapter hdmi;
+            if(vgaMonitor){
+                hdmi = new HdmiToVgaAdapter();
+            } else {
+                hdmi = new HdmiAdapter();
+            }
+
+            var data = new byte[]{};
+            Adapter.Display(data);
+        }
+    }
+}
