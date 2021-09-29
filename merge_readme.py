87ae66f8e73f66411249ebc344f0dde9f44e6e3a
chulhee23 <chulhee2351@gmail.com>
2021-09-29 15:58:02 +0900

    목차 링크

--- merge_readme.py
+++ merge_readme.py
@@ -13,22 +13,20 @@ with open('README.md', 'w') as outfile:
                     with open(readme) as infile:
                         for line in infile:
                             contents.append(line)
-                            # outfile.write(line)
                         
                         infile.seek(0)
                         first_line = infile.readline().rstrip()
-                        
                         table_of_contents.append(first_line)
-
                         contents.append("\n\n\n")
-                        #  outfile.write("\n\n\n")
     
     # 목차 생성
     outfile.seek(0)
     outfile.write("# 목차 \n\n")
     
     for readme_head in table_of_contents:
-        outfile.write(f"- [{readme_head[2:]}](#{readme_head[2:]}) \n")
+        head = readme_head[2:]
+        anchor = readme_head[2:].replace(" ", "-")
+        outfile.write(f"- [{head}](#{anchor}) \n")
 
     outfile.write("\n\n")
     for content in contents:
